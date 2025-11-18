31381340b9470a75f5c70ed13e529daee6ddcb1a
mlane <mlane@travelpassgroup.com>
2025-11-18 05:33:02 -0700

    Add coverage configuration to .gitignore, update package.json and package-lock.json for vitest coverage, and implement flattenSuggestions tests

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,5 +1,5 @@
 import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest'
-import { debounce } from '../utils'
+import { debounce, flattenSuggestions } from '../utils'
 
 describe('utils', () => {
   describe('debounce', () => {
@@ -46,7 +46,65 @@ describe('utils', () => {
     })
   })
 
-  describe.skip('flattenSuggestions', () => {})
+  describe('flattenSuggestions', () => {
+    const mockPlacePrediction: google.maps.places.AutocompleteSuggestion['placePrediction'] =
+      {
+        placeId: 'PLACE_ID',
+        distanceMeters: 100,
+        mainText: {
+          matches: [],
+          text: 'MAIN_TEXT',
+        },
+        secondaryText: {
+          matches: [],
+          text: 'SECONDARY_TEXT',
+        },
+        text: {
+          matches: [],
+          text: 'TEXT',
+        },
+        toPlace: vi.fn(),
+        types: ['geocode'],
+      }
+    const mockSuggestions: google.maps.places.AutocompleteSuggestion[] = [
+      {
+        placePrediction: mockPlacePrediction,
+      },
+    ]
+    const mockFlattenedSuggestions: google.maps.places.AutocompleteSuggestion['placePrediction'][] =
+      [mockPlacePrediction]
+
+    test('should return an empty array when given no suggestions', () => {
+      expect(flattenSuggestions([])).toEqual([])
+    })
+
+    test('should keep existing placePrediction fields', () => {
+      expect(flattenSuggestions(mockSuggestions)).toEqual(
+        mockFlattenedSuggestions
+      )
+    })
+
+    test('should not flatten FormattableText fields when they are missing', () => {
+      const mockPlacePredictionWithoutFormattableText: google.maps.places.AutocompleteSuggestion['placePrediction'] =
+        {
+          ...mockPlacePrediction,
+          mainText: null,
+          secondaryText: null,
+          toPlace: mockPlacePrediction.toPlace,
+        }
+      const mockSuggestionsWithoutFormattableText: google.maps.places.AutocompleteSuggestion[] =
+        [
+          {
+            placePrediction: mockPlacePredictionWithoutFormattableText,
+          },
+        ]
+      const mockFlattenedSuggestionsWithoutFormattableText: google.maps.places.AutocompleteSuggestion['placePrediction'][] =
+        [mockPlacePredictionWithoutFormattableText]
+      expect(flattenSuggestions(mockSuggestionsWithoutFormattableText)).toEqual(
+        mockFlattenedSuggestionsWithoutFormattableText
+      )
+    })
+  })
 
   describe.skip('getGooglePlacesGeocode', () => {})
 
